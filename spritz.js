e740c5150c65271651914ad5f7b6d058c251d53b
JannieP <jannie@pieterse.me>
2014-03-17 21:11:27 +0200

    Update spritz.js

--- spritz.js
+++ spritz.js
@@ -57,7 +57,7 @@ function hide_spritz(){
 // Gets the WPM and the selected text, if any.
 function spritz(){
 
-    var wpm = parseInt($("#spritz_selector").val(), 10);
+    var wpm = parseInt(document.getElementById("spritz_selector").value, 10);
     if(wpm < 1){
         return;
     }
