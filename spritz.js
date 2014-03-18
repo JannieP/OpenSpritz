9a4c9e5d73d5b050780884869cc48822d915a572
JannieP <jannie@pieterse.me>
2014-03-18 16:06:44 +0200

    Update spritz.js

--- spritz.js
+++ spritz.js
@@ -107,7 +107,7 @@ function spritzify(input){
     for (var i=0; i<all_words.length; i++){
 
         // Double up on long words and words with commas.
-        if((all_words[i].indexOf(',') != -1 || all_words[i].indexOf(':') != -1 || all_words[i].indexOf('-') != -1 || all_words[i].indexOf('(') != -1|| all_words[i].length > 8) && all_words[i].indexOf('.') == -1){
+        if(all_words[i].indexOf('.') != -1 || all_words[i].indexOf(',') != -1 || all_words[i].indexOf(':') != -1 || all_words[i].indexOf('-') != -1 || all_words[i].indexOf('(') != -1 || all_words[i].length > 8){
             temp_words.splice(t+1, 0, all_words[i]);
             temp_words.splice(t+1, 0, all_words[i]);
             t++;
@@ -116,9 +116,9 @@ function spritzify(input){
 
         // Add an additional space after punctuation.
         if(all_words[i].indexOf('.') != -1 || all_words[i].indexOf('!') != -1 || all_words[i].indexOf('?') != -1 || all_words[i].indexOf(':') != -1 || all_words[i].indexOf(';') != -1|| all_words[i].indexOf(')') != -1){
-            temp_words.splice(t+1, 0, ".");
-            temp_words.splice(t+1, 0, ".");
-            temp_words.splice(t+1, 0, ".");
+            temp_words.splice(t+1, 0, " ");
+            temp_words.splice(t+1, 0, " ");
+            temp_words.splice(t+1, 0, " ");
             t++;
             t++;
             t++;
