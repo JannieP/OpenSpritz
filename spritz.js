62c28129cd49bb913fb5f5beb0f32884e6998b3f
JannieP <jannie@pieterse.me>
2014-03-19 10:20:33 +0200

    Update spritz.js

--- spritz.js
+++ spritz.js
@@ -340,10 +340,10 @@ function clearTimeouts(){
         window.clearTimeout(id);
     }
     
-    id = window.setInterval(function() {}, 0);
-    while (id--) {
-        window.clearInterval(id);
-    }
+//    id = window.setInterval(function() {}, 0);
+//    while (id--) {
+//         window.clearInterval(id);
+//    }
     
 }
 
